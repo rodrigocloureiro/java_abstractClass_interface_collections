91932424572a414b130d4abf867f458aca27ba9f
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 18:29:24 -0300

    classe Conta codificada

--- src/Conta.java
+++ src/Conta.java
@@ -1,2 +1,17 @@
 public class Conta {
+    private double saldo;
+
+    public void sacar(double valor) {
+        if (valor <= this.saldo) this.saldo -= valor;
+        else System.out.println("Saldo insuficiente..");
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) this.saldo += valor;
+        else System.out.println("Valor inválido..");
+    }
+
+    public double obterSaldo() {
+        return this.saldo;
+    }
 }
