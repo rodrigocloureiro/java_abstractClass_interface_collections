65cfe68fd383d59825e70540ae3ee84214b5839e
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 18:41:48 -0300

    listas de contas e seguros criadas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<Conta> contas = new ArrayList<>();
+        ArrayList<SeguroDeVida> seguros = new ArrayList<>();
     }
 }
