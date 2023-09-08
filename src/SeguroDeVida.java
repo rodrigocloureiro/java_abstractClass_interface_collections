1ea6ea6cdfd38860fb3761553f9acd2e48211a9f
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 20:10:10 -0300

    atributo nome adicionado na SeguroDeVida

--- src/SeguroDeVida.java
+++ src/SeguroDeVida.java
@@ -1,5 +1,10 @@
 public class SeguroDeVida implements Tributavel {
     private final double taxa = 42.00;
+    private String nome;
+
+    public String getNome() {
+        return this.nome;
+    }
 
     @Override
     public double calculaTributos() {
