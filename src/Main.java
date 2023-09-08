7f34f892a5eb6bb77c646dd88acb92e85ab1e558
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 20:28:21 -0300

    imprimindo nome, saldo e tributo dos clientes tributaveis

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         ArrayList<SeguroDeVida> seguros = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
-            contas.add(new ContaCorrente((Math.floor(Math.random() * 2000) + 3000), String.format("Nome%.1f", Math.floor(Math.random() * 2000) + 3000)));
-            contas.add(new ContaPoupanca((Math.floor(Math.random() * 2000) + 3000), String.format("Nome%.1f", Math.floor(Math.random() * 2000) + 3000)));
-            seguros.add(new SeguroDeVida(String.format("%.1f", Math.floor(Math.random() * 2000) + 3000)));
+            contas.add(new ContaCorrente((Math.floor(Math.random() * 2000) + 3000), String.format("Nome%d", (int) Math.floor(Math.random() * 2000) + 3000)));
+            contas.add(new ContaPoupanca((Math.floor(Math.random() * 2000) + 3000), String.format("Nome%d", (int) Math.floor(Math.random() * 2000) + 3000)));
+            seguros.add(new SeguroDeVida(String.format("Nome%d", (int) Math.floor(Math.random() * 2000) + 3000)));
         }
 
         ArrayList<Tributavel> tributaveis = new ArrayList<>();
@@ -22,9 +22,13 @@ public class Main {
         tributaveis.addAll(seguros);
         tributaveis.sort(comparing(Tributavel::calculaTributos).reversed());
 
-        // Imprimindo os tributos em ordem decrescente e o tipo da instância
         for (Tributavel tributavel : tributaveis) {
-            System.out.printf("R$ %.2f - %s\n", tributavel.calculaTributos(), tributavel.getClass().getName());
+            if (tributavel instanceof Conta) {
+                System.out.printf("Nome: %s - Saldo: R$ %.2f - ", ((Conta)tributavel).getNome(), ((Conta)tributavel).obterSaldo());
+            } else if (tributavel instanceof SeguroDeVida) {
+                System.out.printf("Nome: %s - ", ((SeguroDeVida)tributavel).getNome());
+            }
+            System.out.printf("Tributo: R$ %.2f\n", tributavel.calculaTributos());
         }
     }
 }
