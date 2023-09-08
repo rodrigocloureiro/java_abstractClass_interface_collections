544da4d796a066ba062d66214d78a24936f6de4e
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 19:08:55 -0300

    populando a lista de tributaveis e ordenando em ordem decrescente

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import static java.util.Comparator.comparing;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,11 +7,21 @@ public class Main {
         ArrayList<SeguroDeVida> seguros = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
-            contas.add(new ContaCorrente(Math.floor(Math.random() * 1000) + 1000));
-            contas.add(new ContaPoupanca(Math.floor(Math.random() * 1000) + 1000));
+            contas.add(new ContaCorrente(Math.floor(Math.random() * 2000) + 3000));
+            contas.add(new ContaPoupanca(Math.floor(Math.random() * 2000) + 3000));
             seguros.add(new SeguroDeVida());
         }
 
         ArrayList<Tributavel> tributaveis = new ArrayList<>();
+        tributaveis.addAll(
+                contas.stream()
+                        .filter(conta -> conta instanceof Tributavel)
+                        .map(conta -> (Tributavel) conta)
+                        .toList()
+        );
+        tributaveis.addAll(seguros);
+        tributaveis.sort(comparing(Tributavel::calculaTributos).reversed());
+
+
     }
 }
