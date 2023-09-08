95d73da5053c65153b29e31328550d81237f0961
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 18:44:11 -0300

    construtor da classe ContaCorrente criado

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,4 +1,7 @@
 public class ContaCorrente extends Conta implements Tributavel{
+    public ContaCorrente(double saldo) {
+        super(saldo);
+    }
     @Override
     public double calculaTributos() {
         return obterSaldo() * 0.01;
