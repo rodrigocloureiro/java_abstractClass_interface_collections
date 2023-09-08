130fe1f560c5f8113bf62b7c1c0fd75ecb480162
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-08 18:13:55 -0300

    classe ContaCorrente codificada

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,2 +1,6 @@
-public class ContaCorrente {
+public class ContaCorrente extends Conta implements Tributavel{
+    @Override
+    public double calculaTributos() {
+        return obterSaldo() * 0.01;
+    }
 }
